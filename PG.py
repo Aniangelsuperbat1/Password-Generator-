91298030265cbd837d94eb7a0ac91f106e272ce0
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-06 12:47:43 -0500

    Combined all elements into one.

--- PG.py
+++ PG.py
@@ -2,9 +2,22 @@ import random
 
 capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-lower = capital.lower
+lower = capital.lower()
 
 nums = "0123456789"
 
-special = "!@#$%^&*()-+"
+special = "!@#$%^&*()-+<>?/"
+
+uppercase, lowercase, numbers, SP = True, True, True, True
+
+all = ""
+
+if uppercase:
+    all += capital
+if lowercase:
+    all += lower
+if numbers:
+    all += nums
+if SP:
+    all += special
 
